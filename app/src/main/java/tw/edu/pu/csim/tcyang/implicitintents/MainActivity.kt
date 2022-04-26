dd4bdbda3c1ca8d5ea40fe94d5055390896ff5d5
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-04-26 15:24:11 +0800

    v.2 Implicit

--- app/src/main/java/tw/edu/pu/csim/tcyang/implicitintents/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/implicitintents/MainActivity.kt
@@ -1,5 +1,8 @@
 package tw.edu.pu.csim.tcyang.implicitintents
 
+import android.app.SearchManager
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -50,6 +53,32 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
          */
+        when (item.itemId) {
+            R.id.web -> {
+                var it = Intent(Intent.ACTION_VIEW)
+                it.data = Uri.parse("http://www1.pu.edu.tw/~tcyang")
+                startActivity(it)
+            }
+            R.id.email -> {
+                var it = Intent(Intent.ACTION_SENDTO)
+                it.data = Uri.parse("mailto:tcyang@gm.pu.edu.tw")
+                startActivity(it)
+            }
+            R.id.search -> {
+                var it = Intent(Intent.ACTION_WEB_SEARCH)
+                it.putExtra(SearchManager.QUERY, "楊子青")
+                startActivity(it)
+            }
+            R.id.map -> {
+                var it = Intent(Intent.ACTION_VIEW)
+                //it.data = Uri.parse("geo:24.2267756,120.5771591")
+                //it.data = Uri.parse("geo:24.2267756,120.5771591?q=restaurants")
+                //it.data = Uri.parse("geo:0,0?q=台中市沙鹿區台灣大道七段200號")
+                //it.data = Uri.parse("geo:0,0?q=高美濕地")
+                it.data = Uri.parse("google.navigation:q=靜宜大學&mode=d")
+                startActivity(it)
+            }
+        }
         return true
     }
 
